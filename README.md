2f9cdfc4368fae96df8a006ad0c5343a60035421
Anthony Garcia <anthony.garcia@digicatapult.org.uk>
2018-08-13 17:28:53 +0100

    Update README and env

---
+++ README.md
@@ -0,0 +1,47 @@
+# DFKI Adapter
+
+Receive the sensor data from Kerlink and convert them to match DFKI API
+
+## Dependencies
+
+## Usage
+
+The first step is to clone this repo.
+The service can then be run either using `npm` or `docker`.
+
+### With NPM
+
+This was tested using Node LTS (8.11.3) and npm@6.3.0
+
+For production mode:
+
+    # from the project root folder
+    npm install --production
+    npm start
+
+For development:
+
+    # from the project root folder
+    npm install
+    npm run dev
+
+### With docker
+
+    # from the project root folder
+    # This will run the tests and build the project in production mode
+    docker build -t dfki-adapter
+    # This will start the project on port 3000 by default
+    docker run dfki-adapter
+
+### Environment
+
+The app can be configured using the environment.
+The following variables are accepted:
+
+- `ADWISAR_ENDPOINT` (required): the URL of the ADWISAR instance.
+- `PANEL1_DEV_EUI` (default: 4883c7df30051526): The EUI of the device placed at Panel1
+- `PANEL2_DEV_EUI` (default: 4883c7df3005179e): The EUI of the device placed at Panel2
+- `CAP1_DEV_EUI` (default: 4883c7df3005148c): The EUI of the device placed at Cap1
+- `HOST` (default: 0.0.0.0): The host the http server will listen to.
+- `PORT` (default: 3000): The port the http server will bind to.
+- `LOG_LEVEL` (default: info, devDefault: debug): The log level, must be one of debug, info, warn, error or silent.
--- app/env.js
+++ app/env.js
@@ -1,17 +1,52 @@
 const envalid = require('envalid')
 
-const { host, port, str, url } = envalid
+const { host, port, url } = envalid
+
+const devEui = envalid.makeValidator(str => {
+  if (/^[A-Fa-f0-9]{16}$/.test(str)) {
+    return str.toLowerCase()
+  } else {
+    throw new Error(`${str} is not a valid devEui`)
+  }
+})
+
+const logLevel = envalid.makeValidator(str => {
+  if (/^(debug|info|warn|error|silent)$/.test(str)) {
+    return str.toLowerCase()
+  } else {
+    throw new Error(`${str} is not a valid log level`)
+  }
+})
 
 module.exports = envalid.cleanEnv(
   process.env,
   {
-    HOST: host({ default: '0.0.0.0' }),
-    PORT: port({ default: 3000 }),
-    LOG_LEVEL: str({ default: 'info', devDefault: 'debug' }),
-    ADWISAR_ENDPOINT: url(),
-    PANEL1_DEV_EUI: str({ default: '4883c7df30051526' }),
-    PANEL2_DEV_EUI: str({ default: '4883c7df3005179e' }),
-    CAP1_DEV_EUI: str({ default: '4883c7df3005148c' }),
+    HOST: host({
+      default: '0.0.0.0',
+      desc: 'The host the http server will listen to',
+    }),
+    PORT: port({
+      default: 3000,
+      desc: 'The port the http server will bind to',
+    }),
+    LOG_LEVEL: logLevel({
+      default: 'info',
+      devDefault: 'debug',
+      desc: 'The log level, must be one of debug, info, warn, error or silent',
+    }),
+    ADWISAR_ENDPOINT: url({ desc: 'the URL of the ADWISAR instance' }),
+    PANEL1_DEV_EUI: devEui({
+      default: '4883c7df30051526',
+      desc: 'The EUI of the device placed at Panel1',
+    }),
+    PANEL2_DEV_EUI: devEui({
+      default: '4883c7df3005179e',
+      desc: 'The EUI of the device placed at Panel2',
+    }),
+    CAP1_DEV_EUI: devEui({
+      default: '4883c7df3005148c',
+      desc: 'The EUI of the device placed at Cap1',
+    }),
   },
   {
     strict: true,
